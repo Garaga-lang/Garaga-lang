948db3d4aa425096b029132d4b1d15eb1f6593dd
davidleandro-w <dlwibisono.dl@gmail.com>
2020-04-08 23:07:40 +0700

    membuat class pemisahan Lexer, token, dan literals

--- lexer.py
+++ lexer.py
@@ -2,3 +2,10 @@
 #membuat lexer
 #mengambil kelas Lexer di dalam Library sly
 from sly import Lexer
+
+#membuat kelas untuk lexer garaga-lang
+#lexer digunakan untuk memisahkan text ke dalam token
+class pemisahanLexer(Lexer):
+    #membuat nama-nama token (sangat diperlukan)
+    tokens   = {NAMA, ANGKA, KATA, CETAK, JIKA, KAJI, KETIKA, MAKA, UNTUK, HINGGA, FUNGSI, DOBEQ}
+    literals = { '=', '+', '-', '/', '*', '(', ')', ',', ';', '{', '}', '<', '>'}
