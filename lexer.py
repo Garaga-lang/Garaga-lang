071b2e3964166bf53ad6efebbade3e5ae45c83fd
davidleandro-w <dlwibisono.dl@gmail.com>
2020-04-08 23:08:39 +0700

    #membuat karakter yang akan ditolak token

--- lexer.py
+++ lexer.py
@@ -9,3 +9,8 @@ class pemisahanLexer(Lexer):
     #membuat nama-nama token (sangat diperlukan)
     tokens   = {NAMA, ANGKA, KATA, CETAK, JIKA, KAJI, KETIKA, MAKA, UNTUK, HINGGA, FUNGSI, DOBEQ}
     literals = { '=', '+', '-', '/', '*', '(', ')', ',', ';', '{', '}', '<', '>'}
+    
+    #membuat karakter yang akan ditolak token
+    ignore = ' \t'              #mengabaikan spaso
+    ignore_comment = r'\#.*'    #mengabaikan komentar
+    ignore_newline = r'\n+'     #mengabaikan perintah \n
